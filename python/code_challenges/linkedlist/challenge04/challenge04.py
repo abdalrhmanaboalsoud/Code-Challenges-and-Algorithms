78c399fb7fd58afc541b741d8697ef5e7667f017
abdalrhmanaboalsoud <abdalrhmanwaleed1999@gmail.com>
2024-06-28 02:52:49 +0300

    printed  the reversed linkedlist after calling the method.

--- python/code_challenges/linkedlist/challenge04/challenge04.py
+++ python/code_challenges/linkedlist/challenge04/challenge04.py
@@ -1,21 +1,52 @@
 class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
+    def __init__(self):
+        self.head = None
 
-  def reverse_linked_list(self):
-    """
-    Reverses the linked list iteratively.
-    """
-    prev = None
-    curr = self.head
-    while curr:
-      next_node = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next_node
-    self.head = prev
+    def reverse_linked_list(self):
+        prev = None
+        curr = self.head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        self.head = prev
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def print_linked_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Example usage:
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+
+    print("Original linked list:")
+    ll.print_linked_list()
+
+    # Reverse the linked list
+    ll.reverse_linked_list()
+
+    print("Reversed linked list:")
+    ll.print_linked_list()
